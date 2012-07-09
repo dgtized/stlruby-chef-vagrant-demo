47ef48e792ff6fc8c51a716c47ab06f47dc3885d
Charles Comstock <dgtized@gmail.com>
2012-07-09 18:33:44 -0500

    added cookbooks for denyhosts, ntp, openssh

---
+++ cookbooks/denyhosts/metadata.rb
@@ -0,0 +1,4 @@
+maintainer        "Christopher Peplin"
+maintainer_email  "chris.peplin@rhubarbtech.com"
+description       "Install and configure DenyHosts"
+version           "0.1"
---
+++ cookbooks/denyhosts/recipes/default.rb
@@ -0,0 +1,20 @@
+package "denyhosts" do
+  action :install
+end
+
+service "denyhosts" do
+  supports :restart => true
+  action [ :enable, :start ]
+end
+
+cookbook_file "/etc/hosts.allow" do
+  source "hosts.allow"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "denyhosts")
+end
+
+service "denyhosts" do
+  action :start
+end
